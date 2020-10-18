129ae26c5e02107e5d3116e84a876d95557db10a
martyna-dz <martynadziamara14@gmail.com>
2020-10-18 20:25:49 +0200

    zmiana

--- Lista1/src/main/java/ls1/Kalkulator.java
+++ Lista1/src/main/java/ls1/Kalkulator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Kalkulator {
 
-  public static void main (String[] args) {
+  public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     String liczba1 = scanner.nextLine();
     double liczba1_double;	
